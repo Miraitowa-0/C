d3df65cebe544314040a8801753561fe81ceb1fe
Miraitowa-0 <1985888255@qq.com>
2023-01-27 19:14:26 +0800

    牛客网刷题1.0

---
+++ test_1_27/test.c
@@ -0,0 +1,66 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//牛客网刷题1.0
+// 输入描述：输入一个char类型字符
+//输出描述：输出一个用这个字符填充的对角线长5个字符，倾斜放置的菱形
+//输入：#
+//输出：
+//  #
+// ###
+//#####
+// ###
+//  #
+//int main() 
+//{
+//    char input = 0;
+//    scanf("%c", &input);
+//    int n = 3;
+//    int i, j = 0;
+//    //打印上半部分
+//    for (i = 1; i <= n; i++)
+//    {
+//        //打印空格
+//        for (j = 0; j < n - i; j++)
+//        {
+//            printf(" ");
+//        }
+//        //打印#
+//        for (j = 0; j < 2 * i - 1; j++)
+//        {
+//            printf("%c", input);
+//        }
+//        printf("\n");
+//    }
+//    //打印下半部分
+//    for (i = 1; i < n; i++)
+//    {
+//        //打印空格
+//        for (j = 0; j < i; j++)
+//        {
+//            printf(" ");
+//        }
+//        //打印#
+//        for (j = 0; j < 2*(n-i)-1; j++)
+//        {
+//            printf("%c", input);
+//        }
+//        printf("\n");
+//    }
+//    return 0;
+//}
+
+
+//输入描述：一行，一个字符。
+//输出描述：一行，输出输入字符对应的ASCII码。
+//示例1
+//输入：c
+//输出：99
+//int main() {
+//    char input = 0;
+//    scanf("%c", &input);
+//    printf("%d", input);
+//    return 0;
+//}
+
+
+
